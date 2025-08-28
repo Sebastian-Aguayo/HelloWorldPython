88f72b820bea9fa3d962eea73790b13b9afea178
Sebastian Aguayo <SebastianAguayoForteza@gmail.com>
2025-08-27 23:07:03 -0400

    Mi primer HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#This program print helloworld
+hi = "HelloWorld Sebastian" 
+print(hi)
+
+#add two numbers
+number_a = 1592
+number_b = 4500
+total = number_a + number_b
+print("La suma es:",total)
+
+#substract 1000 to the total
+numberminus = total - 1000
+print("El total es ",total," - 1000 es: ",numberminus ) 
